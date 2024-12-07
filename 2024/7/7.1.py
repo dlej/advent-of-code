dd18e08715f05a940d2067bef8d4c6fda2276593
Daniel LeJeune <16905520+dlej@users.noreply.github.com>
2024-12-07 09:03:05 -0500

    Day 7

---
+++ 2024/7/7.1.py
@@ -0,0 +1,30 @@
+from itertools import product
+from operator import add, mul
+
+with open("7.txt", "r") as f:
+    lines = f.readlines()
+
+equations = []
+for line in lines:
+    test_value, nums = line.split(":")
+    test_value = int(test_value)
+    nums = [int(x) for x in nums.strip().split(" ")]
+    equations.append((test_value, nums))
+
+
+def eval_ops(nums, ops):
+    nums = list(nums)
+    s = nums.pop(0)
+    for op, num in zip(ops, nums):
+        s = op(s, num)
+    return s
+
+
+s = 0
+for test_value, nums in equations:
+    for ops in product(*[[add, mul]] * (len(nums) - 1)):
+        if test_value == eval_ops(nums, ops):
+            s += test_value
+            break
+
+print(s)
